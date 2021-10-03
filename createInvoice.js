f7b7743432300fbad1f1734c0f98cf0306e17f42
Lounès Ksouri <dev@louneskmt.com>
2021-10-03 21:26:22 +0200

    Add Tor support

--- createInvoice.js
+++ createInvoice.js
@@ -1,6 +1,9 @@
 const { LND } = require('./backend')
 const axios = require('axios')
 const crypto = require('crypto')
+const { SocksProxyAgent } = require('socks-proxy-agent')
+
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = '0';
 
 const createInvoice = async (backend, metadata, msat) => {
     switch (backend) {
@@ -10,16 +13,27 @@ const createInvoice = async (backend, metadata, msat) => {
 }
 
 const _getLndInvoice = async (metadata, msat) => {
-    const invoiceResult = await axios.post(process.env.LIGESS_LND_REST + '/v1/invoices',
-        {
+    const requestConfig = {
+        url: process.env.LIGESS_LND_REST + '/v1/invoices',
+        method: 'POST',
+        data: {
             value_msat: msat,
             description_hash: crypto.createHash('sha256').update(metadata).digest('base64')
         },
-        {
-            headers: {
-                'Grpc-Metadata-macaroon': process.env.LIGESS_LND_MACAROON
-            }
-        })
+        headers: {
+            'Grpc-Metadata-macaroon': process.env.LIGESS_LND_MACAROON
+        },
+        rejectUnauthorized: false
+    }
+
+    if (process.env.LIGESS_TOR_PROXY_URL) {
+        const agent = new SocksProxyAgent(process.env.LIGESS_TOR_PROXY_URL)
+        requestConfig.httpAgent = agent
+        requestConfig.httpsAgent = agent
+    }
+
+    const invoiceResult = await axios(requestConfig)
+
     return invoiceResult.data.payment_request
 }
 
